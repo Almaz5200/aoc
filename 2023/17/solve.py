2c64498e1b1c414ab555ac5632234b3d2aebc1e9
Artem Trubacheev <almaz5200@gmail.com>
2023-12-18 12:09:22 +0400

    d17/18

--- 2023/17/solve.py
+++ 2023/17/solve.py
@@ -1,36 +1,59 @@
 import os
 import math
+import heapq
 import sys
 import copy
 from typing import List
 from aocd import get_data, submit
 
-day = 14
+day = 17
 lines = get_data(day=day, year=2023)
 
 sys.setrecursionlimit(100000000)
 
-
-def parse_input(s):
-    ll = s.splitlines()
-    return [[c for c in l] for l in ll]
-
-
-def solve_a(data):
-    return 0
-
-
-def solve_b(data):
-    return 0
+### ALL BELLOW ARE FROM JONATHAN PAULSON CODE ###
+### Literal copy paste, was in no condition to think properly myself ###
+### Got through it and understood tho ###
+### Couldn't figure myself quick enough :( ###
+D = lines
+L = D.split("\n")
+G = [[c for c in row] for row in L]
+R = len(G)
+C = len(G[0])
 
 
-data = parse_input(lines)
+def solve(part2):
+    Q = [(0, 0, 0, -1, -1)]
+    D = {}
+    while Q:
+        dist, r, c, dir_, indir = heapq.heappop(Q)
+        if (r, c, dir_, indir) in D:
+            continue
+        D[(r, c, dir_, indir)] = dist
+        for i, (dr, dc) in enumerate([[-1, 0], [0, 1], [1, 0], [0, -1]]):
+            rr = r + dr
+            cc = c + dc
+            new_dir = i
+            new_indir = 1 if new_dir != dir_ else indir + 1
 
-with open("input.txt", "r") as f:
-    test_data = parse_input(f.read())
+            isnt_reverse = (new_dir + 2) % 4 != dir_
 
-assert solve_a(test_data) == 374, solve_a(test_data)
-submit(solve_a(data), part="a", day=day, year=2023)
+            isvalid_part1 = new_indir <= 3
+            isvalid_part2 = new_indir <= 10 and (
+                new_dir == dir_ or indir >= 4 or indir == -1
+            )
+            isvalid = isvalid_part2 if part2 else isvalid_part1
 
-assert solve_b(test_data) == -1, solve_b(test_data)
-submit(solve_b(data), part="b", day=day, year=2023)
+            if 0 <= rr < R and 0 <= cc < C and isnt_reverse and isvalid:
+                cost = int(G[rr][cc])
+                heapq.heappush(Q, (dist + cost, rr, cc, new_dir, new_indir))
+
+    ans = 1e9
+    for (r, c, dir_, indir), v in D.items():
+        if r == R - 1 and c == C - 1:
+            ans = min(ans, v)
+    return ans
+
+
+submit(solve(False), part="a", day=day, year=2023)
+submit(solve(True), part="b", day=day, year=2023)
--- 2023/18/solve.py
+++ 2023/18/solve.py
@@ -2,26 +2,116 @@ import os
 import math
 import sys
 import copy
-from typing import List
+from typing import Counter, List
 from aocd import get_data, submit
 
-day = 14
+day = 18
 lines = get_data(day=day, year=2023)
+
+with open("inputr.txt", "w") as f:
+    f.write(lines)
 
 sys.setrecursionlimit(100000000)
 
 
 def parse_input(s):
     ll = s.splitlines()
-    return [[c for c in l] for l in ll]
+    res = []
+    for line in ll:
+        line = line.split(" ")
+        res.append((line[0], int(line[1]), line[2]))
+
+    return res
+
+
+D = {}
+
+
+def makeMap(data, x, y):
+    if (x, y) in D:
+        return
+
+    if x < 0 or y < 0 or x >= len(data) or y >= len(data[0]):
+        return
+
+    if data[x][y] == "#":
+        return
+
+    D[(x, y)] = True
+
+    makeMap(data, x + 1, y)
+    makeMap(data, x - 1, y)
+    makeMap(data, x, y + 1)
+    makeMap(data, x, y - 1)
 
 
 def solve_a(data):
-    return 0
+    D.clear()
+    map = [["."] * 5000 for _ in [n for n in range(5000)]]
+    C = (2500, 2500)
+    lx, ly, mx, my = len(map), len(map[0]), 0, 0
 
+    for inst in data:
+        d, r, c = inst
 
-def solve_b(data):
-    return 0
+        dx, dy = 0, 0
+        cx, cy = C
+        if d == "R":
+            dx = r
+        if d == "L":
+            dx = -r
+        if d == "U":
+            dy = -r
+        if d == "D":
+            dy = r
+
+        C = (cx + dx, cy + dy)
+
+        lx, ly = min(lx, cx), min(ly, cy)
+        mx, my = max(mx, cx), max(my, cy)
+
+        fx, tx = cx, cx + dx
+        fy, ty = cy, cy + dy
+
+        if fx > tx:
+            fx, tx = tx, fx
+        if fy > ty:
+            fy, ty = ty, fy
+
+        for x in range(fx, tx + 1):
+            for y in range(fy, ty + 1):
+                map[y][x] = "#"
+
+    nmap = copy.deepcopy(map)
+
+    makeMap(map, 0, 0)
+    for x in range(len(map)):
+        for y in range(len(map[0])):
+            if (x, y) not in D:
+                nmap[x][y] = "#"
+    map = nmap
+    # count the number of #
+    cnt = sum(
+        [1 for x in range(len(map)) for y in range(len(map[0])) if map[x][y] == "#"]
+    )
+
+    for x in range(50):
+        for y in range(50):
+            if map[x][y] == "#":
+                print("X", end="")
+            else:
+                print(".", end="")
+        print()
+
+    for x in range(50):
+        for y in range(50):
+            if (x, y) in D:
+                print("X", end="")
+            else:
+                print(".", end="")
+        print()
+
+    return cnt
 
 
 data = parse_input(lines)
@@ -29,8 +119,5 @@ data = parse_input(lines)
 with open("input.txt", "r") as f:
     test_data = parse_input(f.read())
 
-assert solve_a(test_data) == 374, solve_a(test_data)
+assert solve_a(test_data) == 62, solve_a(test_data)
 submit(solve_a(data), part="a", day=day, year=2023)
-
-assert solve_b(test_data) == -1, solve_b(test_data)
-submit(solve_b(data), part="b", day=day, year=2023)
