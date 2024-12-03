c958351bc775f3cd5435d335b87c147272b1167f
Artem Trubacheev <almaz5200@gmail.com>
2024-12-03 06:18:57 +0100

    d3

--- 2024/3/solve.py
+++ 2024/3/solve.py
@@ -5,34 +5,57 @@ import copy
 from typing import List
 from aocd import get_data, submit
 from collections import defaultdict, deque, Counter
+import re
 
-from aocd.models import json
-
-day = 2
+day = 3
 lines = get_data(day=day, year=2024)
 
 sys.setrecursionlimit(100000000)
 
 
 def prep_data(raw_data):
-    return raw_data.split("\n")
+    return raw_data
 
 
 def solve_a(raw_data):
     data = prep_data(raw_data)
-    return 0
+
+    pattern = r"mul\(([0-9]{1,3}),([0-9]{1,3})\)"
+    ans = 0
+    for match in re.findall(pattern, data):
+        print(match)
+        ans += int(match[0]) * int(match[1])
+
+
+    print(ans)
+    return ans
 
 
 def solve_b(raw_data):
     data = prep_data(raw_data)
-    return 0
+
+    pattern = r"do\(\)|don't\(\)|mul\(([0-9]{1,3}),([0-9]{1,3})\)"
+    regex = re.compile(pattern)
+    ans = 0
+    isActive = True
+    for match in regex.finditer(data):
+        if match.group(0) == "do()":
+            isActive=True
+        elif match.group(0) == "don't()": 
+            isActive = False
+        elif isActive:
+            ans += int(match.group(1)) * int(match.group(2))
+
+
+    print(ans)
+    return ans
 
 
 with open("test_input.txt", "r") as f:
     test_data = f.read()
 
-# assert solve_a(test_data) == 11, solve_a(test_data)
+assert solve_a(test_data) == 161, solve_a(test_data)
 submit(solve_a(lines), part="a", day=day, year=2024)
 
-assert solve_b(test_data) == 31, solve_b(test_data)
+assert solve_b(test_data) == 48, solve_b(test_data)
 submit(solve_b(lines), part="b", day=day, year=2024)
